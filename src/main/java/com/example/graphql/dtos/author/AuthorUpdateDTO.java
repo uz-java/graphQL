31ea1a798eaf3be314555b00bba4855dc4f8d8fb
asliddin <asliddintojaliyev88@gmail.com>
2022-10-13 18:38:51 +0500

    commit

---
+++ src/main/java/com/example/graphql/dtos/author/AuthorUpdateDTO.java
@@ -0,0 +1,9 @@
+package com.example.graphql.dtos.author;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 13/10/22 18:38 (Thursday)
+ * graphQL/IntelliJ IDEA
+ */
+public class AuthorUpdateDTO {
+}
