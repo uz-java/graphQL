47ab332443ac36cacf1cdbfb0ccdb0f31a66a935
asliddin <asliddintojaliyev88@gmail.com>
2022-10-13 19:13:19 +0500

    commit

---
+++ src/main/java/com/example/graphql/service/author/AuthorService.java
@@ -0,0 +1,9 @@
+package com.example.graphql.service.author;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 13/10/22 19:12 (Thursday)
+ * graphQL/IntelliJ IDEA
+ */
+public class AuthorService {
+}
---
+++ src/main/java/com/example/graphql/service/book/BookService.java
@@ -0,0 +1,9 @@
+package com.example.graphql.service.book;
+
+/**
+ * @author "Tojaliyev Asliddin"
+ * @since 13/10/22 19:13 (Thursday)
+ * graphQL/IntelliJ IDEA
+ */
+public class BookService {
+}
