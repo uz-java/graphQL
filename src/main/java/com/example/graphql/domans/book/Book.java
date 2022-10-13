de538b68af75d8a26c251b80e2e56561a55437ad
asliddin <asliddintojaliyev88@gmail.com>
2022-10-13 18:59:04 +0500

    commit

--- src/main/java/com/example/graphql/domans/book/Book.java
+++ src/main/java/com/example/graphql/domans/book/Book.java
@@ -1,9 +1,26 @@
 package com.example.graphql.domans.book;
 
+import com.example.graphql.enams.Rating;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/10/22 18:25 (Thursday)
  * graphQL/IntelliJ IDEA
  */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Book {
+    @Id
+    private Integer id;
+    private String title;
+    private Integer pages;
+    private Rating rating;
 }
--- src/main/java/com/example/graphql/enams/Rating.java
+++ src/main/java/com/example/graphql/enams/Rating.java
@@ -1,9 +1,29 @@
 package com.example.graphql.enams;
 
+import com.fasterxml.jackson.annotation.JsonValue;
+
 /**
  * @author "Tojaliyev Asliddin"
  * @since 13/10/22 18:29 (Thursday)
  * graphQL/IntelliJ IDEA
  */
 public enum Rating {
+    FIVE_STARS("⭐️⭐️⭐️⭐️⭐️️️️", "5"),
+    FOUR_STARS("⭐️⭐️⭐️⭐️", "4"),
+    THREE_STARS("⭐️⭐️⭐️", "3"),
+    TWO_STARS("⭐️⭐️", "2"),
+    ONE_STAR("⭐️", "1");
+
+    private String star;
+    private String rating;
+
+    Rating(String star, String rating) {
+        this.star = star;
+        this.rating = rating;
+    }
+
+    @JsonValue
+    public String getStar(){
+        return star;
+    }
 }
